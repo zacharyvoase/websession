73e253eba5d211492963d4f6113b615963ba24c8
Zack Voase <zack@meat.io>
2023-06-16 13:06:22 -0700

    Add ToC and header anchors.

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,15 @@
 const lightningCSS = require('@11tyrocks/eleventy-plugin-lightningcss');
 const markdownIt = require('markdown-it');
-const markdownItAttrs = require('markdown-it-attrs');
-const markdownItHighlightJs = require('markdown-it-highlightjs');
+const markdownAnchor = require('markdown-it-anchor');
 
 const markdown = markdownIt({
   html: true,
   linkify: true,
   typographer: true,
-}).use(markdownItHighlightJs, {})
-  .use(markdownItAttrs, {});
+}).use(require('markdown-it-highlightjs'), {})
+  .use(markdownAnchor, { permalink: markdownAnchor.permalink.headerLink() })
+  .use(require('markdown-it-attrs'), {})
+  .use(require('markdown-it-toc-done-right'), {level: 2});
 
 module.exports = (config) => {
   config.addFilter('smartquotes', (post) => { return smartquotes(post); });
